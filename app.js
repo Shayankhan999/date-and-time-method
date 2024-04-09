bafbf6aa1df838272dc47f5edcbe4d105889d227
Shayankhan <shayankhan9148@gmail.com>
2024-04-09 11:30:07 +0500

    method

---
+++ app.js
@@ -0,0 +1,69 @@
+                                       //STRING METHOD      
+                        //    question 1
+
+// var firstName=prompt("enter your first name");
+// var lastName=prompt("enter your second nam");
+// var fullName = firstName+"  " +lastName
+// console.log(fullName);
+
+                                       //question 2
+
+// var mobileChoice=prompt("favourite mobile phone model") ;
+// var lengthOfInput = mobileChoice.length;
+// document.write("my favourite phone is:"+mobileChoice+ "</br>") ;
+// document.write("lenght of string:"+lengthOfInput)
+
+                                      // question 3
+
+// var word="pakistan";
+// var indexOfN= word.indexOf("n") ;
+// document.write(`string ${word} <br>`);
+// document.write          (`index is:${indexOfN}`)
+
+                                    // question 4
+
+// var word= "hello world";
+// var lastWord= word.lastIndexOf("l");
+// document.write(`string : ${word} <br>`)
+// document.write(`last index of "l" is :${lastWord}`)
+
+                                        // question 5
+
+// var word = "pakistan";
+// var indexOfN = word.charAt("3");
+// document.write(`string :${word} <br>`);
+// document.write(`character in index 3:${indexOfN}`);
+
+                             //       question 6
+
+
+// var firstName = prompt("Enter your first name");
+// var lastName = prompt("Enter your last name");
+// var fullName = firstName.concat(" ", lastName);
+// console.log(fullName);
+
+                                    // question 7
+
+// var word = "Hyderabad";
+// var replacedWord = word.replace("Hyder", "Islam");
+// document.write("Original word: " + word + "<br>");
+// document.write("After replacement: " + replacedWord);
+
+                                 // question 8
+
+// var message = "Ali and Sami are best friends. They play cricket and football together.";
+// var replaced = message.replace(/and/g, "&");
+// document.write(replaced);
+                          
+                              // question 8
+
+//  var str = "472";
+// var num = Number(str);
+// document.write(`value:${str} <br>`)
+// document.write( `value:${typeof str} <br>`)
+// document.write("Converted value: " + num + "<br>");
+// document.write("Type of converted value: " + typeof num);
+                             
+                             // question 9
+
+                             
